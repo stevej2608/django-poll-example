a35a2981a7cb8f4420d437343094f14ac4842fb9
Steve Jones <jonesst2608@gmail.com>
2024-06-03 08:33:30 +0100

    Fix ALL linting errors

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,4 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
 
@@ -8,16 +7,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
-        return self.question_text
+    def __str__(self) -> str:
+        return str(self.question_text)
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+
+    # https://books.agiliq.com/projects/django-admin-cookbook/en/latest/sorting_calculated_fields.html#how-to-enable-sorting-on-calculated-fields
+
+    was_published_recently.admin_order_field = 'pub_date' # type: ignore
+    was_published_recently.boolean = True # type: ignore
+    was_published_recently.short_description = 'Published recently?'# type: ignore
 
 
 class Choice(models.Model):
@@ -26,4 +27,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return str(self.choice_text)
--- polls/views.py
+++ polls/views.py
@@ -40,7 +40,7 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) # type: ignore
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
@@ -49,4 +49,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # type: ignore
