bc5653352c41f3bcf9293946fb61e4325509f275
Steve Jones <jonesst2608@gmail.com>
2024-06-03 08:36:17 +0100

    Fix ALL linting errors

--- polls/tests.py
+++ polls/tests.py
@@ -113,7 +113,7 @@ class QuestionDetailViewTests(TestCase):
         returns a 404 not found.
         """
         future_question = create_question(question_text='Future question.', days=5)
-        url = reverse('polls:detail', args=(future_question.id,))
+        url = reverse('polls:detail', args=(future_question.id,)) # type: ignore
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -123,6 +123,6 @@ class QuestionDetailViewTests(TestCase):
         displays the question's text.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
-        url = reverse('polls:detail', args=(past_question.id,))
+        url = reverse('polls:detail', args=(past_question.id,)) # type: ignore
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
