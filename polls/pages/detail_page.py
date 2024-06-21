95604a813ee79296d24461f13f11645549f27ccd
Steve Jones <jonesst2608@gmail.com>
2024-06-21 06:52:43 +0100

    Can now vote

--- polls/pages/detail_page.py
+++ polls/pages/detail_page.py
@@ -2,13 +2,13 @@ import logging
 from typing import Dict, Any
 from channels.db import database_sync_to_async
 from django.utils import timezone
-from reactpy import component, event, html
-from reactpy_django.hooks import use_query
+from reactpy import component, event, html, use_state
+from reactpy_django.hooks import use_query, use_mutation
 from reactpy_router import use_params, link
 
 from reactpy_forms import create_form, FormModel, use_form_state
 
-from ..models import Question
+from ..models import Question, Choice
 
 from .page_404 import Page_404
 
@@ -36,6 +36,8 @@ def detail():
     model, set_model = use_form_state(ChoiceData())
     Form, Field = create_form(model, set_model)
 
+    error, set_error = use_state('')
+
     params = use_params()
     qs = use_query(get_questions)
 
@@ -45,11 +47,25 @@ def detail():
         return html.h2("Loading...")
 
     @component
-    def SubmitButton(model: FormModel):
+    def SubmitButton(question: Question, choice:int):
+
+        async def _inc_vote():
+            selected_choice = await database_sync_to_async(question.choice_set.get)(pk=choice)
+            selected_choice.votes += 1
+            await database_sync_to_async(selected_choice.save)()
+
+        inc_vote = use_mutation(_inc_vote)
 
         @event(prevent_default=True)
         def onclick(event: EventArgs):
             log.info(model)
+            if choice == 0:
+                set_error("You didn't select a choice.")
+            else:
+                set_error("")
+                inc_vote()
+
+
 
         return html.input({
             'type': 'submit',
@@ -57,6 +73,15 @@ def detail():
             'class_name': 'btn btn-success btn-lg btn-block mt-4',
             'on_click': onclick
             })
+
+
+    @component
+    def error_message(error_message:str):
+        if error_message:
+            return html.p({'class_name': 'alert alert-danger'},
+                html.strong(f"{error_message}")
+            )
+        return ""
 
 
     def choice_field(choice, i):
@@ -78,19 +103,17 @@ def detail():
                     ))
 
     try:
-        question = qs.data[int(params['pk']) - 1]
-        error_message = None
+        question: Question = qs.data[int(params['pk']) - 1]
+
 
         return html.div(
             link("Back To Polls", to='/polls/', class_name='btn btn-secondary btn-sm mb-3'),
             html.h1({'class_name': 'text-center mb-3'}, f"{question.question_text}"),
-            # html.p({'class_name': 'alert alert-danger'},
-            #     html.strong(f"{error_message}")
-            # ),
+            error_message(error),
 
             Form(
                 [choice_field(choice, i+1) for i, choice in enumerate(question.choice_set.all())],
-                SubmitButton(model)
+                SubmitButton(question, model.choice)
 
             )
 
