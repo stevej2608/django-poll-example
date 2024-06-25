ae68b10d9d0075bc5098437744393abba7bcaca8
Steve Jones <jonesst2608@gmail.com>
2024-06-25 06:59:25 +0100

    Added dumpurls command

---
+++ polls/management/commands/dumpurls.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.urls import URLPattern, URLResolver
+
+urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
+
+def list_urls(lis, acc=None):
+    if acc is None:
+        acc = []
+    if not lis:
+        return
+    l = lis[0]
+    if isinstance(l, URLPattern):
+        yield acc + [str(l.pattern)]
+    elif isinstance(l, URLResolver):
+        yield from list_urls(l.url_patterns, acc + [str(l.pattern)])
+    yield from list_urls(lis[1:], acc)
+
+class Command(BaseCommand):
+    help = 'List all URLs from the urlconf'
+
+    def handle(self, *args, **options):
+        for p in list_urls(urlconf.urlpatterns):
+            print(''.join(p))
+
